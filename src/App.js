0a53ed9f1f13f5d56aac62c6cf7a5587d2de433a
Shahinur <shahinurislam30101999@gmail.com>
2023-04-06 04:27:49 +0600

    initial setap design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import ResetPassword from './pages/ResetPassword';
+import ForgetPassword from './pages/ForgetPassword';
+import Dashboard from './pages/Dashboard';
+import MainLayout from './components/MainLayout';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+        <Route path="/forgot-password" element={< ForgetPassword/>} />
+        <Route path="/admin" element={< MainLayout/>}>
+          <Route index element={< Dashboard/>} />
+        </Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
